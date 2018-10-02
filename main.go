8c78bfa83a2f0dc355c7b4c68daa0e78ea55c107
cuu <dexterkidd@gmail.com>
2018-10-03 00:25:02 +0800

    add socket to control enable or disable

--- main.go
+++ main.go
@@ -4,18 +4,19 @@ import (
 	"os"
 	"fmt"
 	"os/exec"
-    "io/ioutil"
-    "strings"
-    "encoding/json"
-    "errors"
-    "log"
-    "strconv"
-    "path/filepath"
-    
-    "github.com/go-ini/ini"
+	"io/ioutil"
+	"strings"
+	"encoding/json"
+	"errors"
+	"log"
+	"strconv"
+	"path/filepath"
+	"net"
+	
+	"github.com/go-ini/ini"
 	"github.com/veandco/go-sdl2/sdl"
-    "github.com/veandco/go-sdl2/ttf"
-
+	"github.com/veandco/go-sdl2/ttf"
+	
 	"github.com/cuu/gogame"
 	"github.com/cuu/gogame/color"
 	"github.com/cuu/gogame/display"
@@ -26,22 +27,22 @@ import (
 	//"github.com/cuu/gogame/image"
 	"github.com/cuu/gogame/font"
 	"github.com/cuu/gogame/time"	
-    
-    "github.com/cuu/LauncherGo/sysgo/easings"
-    "github.com/cuu/LauncherGo/sysgo/UI"
-    
+	
+	"github.com/cuu/LauncherGo/sysgo/easings"
+	"github.com/cuu/LauncherGo/sysgo/UI"
+	
 )
 
 type SDLWindow struct{
-    PosX int
-    PosY int
-    OnScreen bool  // default false== hide
-    Data100 []int
-    Data40  []int
-    win *sdl.Window 
-    screen *sdl.Surface
-    main_font *ttf.Font
-    
+	PosX int
+	PosY int
+	OnScreen bool  // default false== hide
+	Data100 []int
+	Data40  []int
+	win *sdl.Window 
+	screen *sdl.Surface
+	main_font *ttf.Font
+	
     
 }
 
@@ -57,26 +58,29 @@ func (self *JobRespond) String() string{
 }
 
 var (
-    JobMap map[string]string // BashName => String(JobRespond)
-    ALLOW_EXTS=[5]string{".sh",".py",".lsp",".js",".bin"}
-    Width = 320
-    Height = 20
-    SKIP_READ_DIR = 2
-    DELAY_MS = 2000
-    DELAY_FREQ = 30*1000   
-    BGCOLOR  =  &color.Color{0x34,0xb9,0xea,255}
-    TXTCOLOR =  &color.Color{0xff,0xff,0xff,255}
-    FTSIZE = 14
+	JobMap map[string]string // BashName => String(JobRespond)
+	ALLOW_EXTS=[5]string{".sh",".py",".lsp",".js",".bin"}
+	Width = 320
+	Height = 20
+	SKIP_READ_DIR = 2
+	DELAY_MS = 2000
+	DELAY_FREQ = 30*1000   
+	BGCOLOR  =  &color.Color{0x34,0xb9,0xea,255}
+	TXTCOLOR =  &color.Color{0xff,0xff,0xff,255}
+	FTSIZE = 14
+	Enabled = true
 )
 
 const (
 	RUNEVT = 1 
-    EasingDur = 10
-    EasingDelay = 20 //ms
-    GSNOTIFY_CFG="gsnotify.cfg"
+	EasingDur = 10
+	EasingDelay = 20 //ms
+	GSNOTIFY_CFG="gsnotify.cfg"
 )
 
 var done chan bool
+
+var Cfg *ini.File
 
 func ConvertToRGB(hexstr string) *color.Color {
 	if len(hexstr) < 7 || string(hexstr[0]) != "#" { // # 00 00 00
@@ -95,98 +99,142 @@ func ConvertToRGB(hexstr string) *color.Color {
 	return col
 }
 
-func LoadConfig() {
-    if UI.FileExists(GSNOTIFY_CFG) {
-        load_opts := ini.LoadOptions{
-            IgnoreInlineComment:true,
-        }
-        cfg, err := ini.LoadSources(load_opts, GSNOTIFY_CFG )
-        if err != nil {
-            fmt.Printf("Fail to read file: %v\n", err)
-            return
-        }
-        
-        section := cfg.Section("Settings")
-        if section != nil {
-            _opts := section.KeyStrings()
-            for _,v := range _opts {
-                if v == "DELAY_MS" {
-                    val := section.Key(v).String()
-                    i, err := strconv.Atoi(val)
-                    if err == nil {
-                        DELAY_MS = i
-                    }
-                }
-                if v == "DELAY_FREQ" {
-                    val := section.Key(v).String()
-                    i, err := strconv.Atoi(val)
-                    if err == nil {
-                        DELAY_FREQ = i
-                    }
-                }
+func WriteConfig() {
+	
+	if UI.FileExists(GSNOTIFY_CFG) {
 
-                if v == "Width" {
-                    val := section.Key(v).String()
-                    i, err := strconv.Atoi(val)
-                    if err == nil {
-                        Width = i
-                    }
-                }
-                
-                if v == "Height" {
-                    val := section.Key(v).String()
-                    i, err := strconv.Atoi(val)
-                    if err == nil {
-                        Height = i
-                    }
-                }
-                
-                if v == "FTSIZE" {
-                    val := section.Key(v).String()
-                    i, err := strconv.Atoi(val)
-                    if err == nil {
-                        FTSIZE = i
-                    }
-                }
-                                                                
-                if v == "BGCOLOR" {
-                    parsed_color := ConvertToRGB( section.Key(v).String() )
-                    if parsed_color != nil {
-                        BGCOLOR = parsed_color
-                    }
-                }
-                
-                if v == "TXTCOLOR" {
-                    parsed_color := ConvertToRGB( section.Key(v).String() )
-                    if parsed_color != nil {
-                        TXTCOLOR = parsed_color
-                    }
-                }                
-                
-            }
-        }
-    }
+		if Cfg != nil {
+			section := Cfg.Section("Settings")
+
+			if section.HasKey("Enabled") == false {
+				section.NewKey("Enabled","True")
+			}
+			
+			if Enabled == true {
+				section.Key("Enabled").SetValue("True")
+			}
+			if Enabled == false {
+				section.Key("Enabled").SetValue("False")
+			}
+		
+		
+			err:= Cfg.SaveTo(GSNOTIFY_CFG)
+			if err != nil {
+				fmt.Println(err)
+			}
+			
+		}
+		
+	}
+}
+
+
+func LoadConfig() {
+	if UI.FileExists(GSNOTIFY_CFG) {
+		load_opts := ini.LoadOptions{
+			IgnoreInlineComment:true,
+		}
+		var err error
+		
+		Cfg, err = ini.LoadSources(load_opts, GSNOTIFY_CFG )
+		if err != nil {
+			fmt.Printf("Fail to read file: %v\n", err)
+			return
+		}
+		
+		section := Cfg.Section("Settings")
+		if section != nil {
+			_opts := section.KeyStrings()
+			for _,v := range _opts {
+				if v == "DELAY_MS" {
+					val := section.Key(v).String()
+					i, err := strconv.Atoi(val)
+					if err == nil {
+						DELAY_MS = i
+					}
+				}
+				if v == "DELAY_FREQ" {
+					val := section.Key(v).String()
+					i, err := strconv.Atoi(val)
+					if err == nil {
+						DELAY_FREQ = i
+					}
+				}
+				
+				if v == "Width" {
+					val := section.Key(v).String()
+					i, err := strconv.Atoi(val)
+					if err == nil {
+						Width = i
+					}
+				}
+				
+				if v == "Height" {
+					val := section.Key(v).String()
+					i, err := strconv.Atoi(val)
+					if err == nil {
+						Height = i
+					}
+				}
+				
+				if v == "FTSIZE" {
+					val := section.Key(v).String()
+					i, err := strconv.Atoi(val)
+					if err == nil {
+						FTSIZE = i
+					}
+				}
+				
+				if v == "BGCOLOR" {
+					parsed_color := ConvertToRGB( section.Key(v).String() )
+					if parsed_color != nil {
+						BGCOLOR = parsed_color
+					}
+				}
+				
+				if v == "TXTCOLOR" {
+					parsed_color := ConvertToRGB( section.Key(v).String() )
+					if parsed_color != nil {
+						TXTCOLOR = parsed_color
+					}
+				}
+				
+				if v == "Enabled" {
+					if section.Key(v).String() == "True" {
+						Enabled = true
+					}
+
+					if section.Key(v).String() == "False" {
+						Enabled = false
+					}
+				}
+				
+			}
+		}
+	}
 }
 
 func init() {
 
-    dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-    if err != nil {
-            log.Fatal(err)
-    }
-    fmt.Println(dir)
-    os.Chdir(dir)
-        
-    LoadConfig()
-    
-    sdl_window = &SDLWindow{}
-    sdl_window.Data100 = EasingData(0,100)
-    sdl_window.Data40 = EasingData(0,40)
-    
-    JobMap = make(map[string]string)
-    
-    done = make(chan bool)
-    
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
+	os.Chdir(dir)
+	
+	LoadConfig()
+	
+	Enabled = true
+	
+	sdl_window = &SDLWindow{}
+	sdl_window.Data100 = EasingData(0,100)
+	sdl_window.Data40 = EasingData(0,40)
+	
+	JobMap = make(map[string]string)
+	
+	done = make(chan bool)
+	
 }
 
 func EasingData(start,distance int) []int {
@@ -312,90 +360,93 @@ func RunScript(_script_name string) *JobRespond {
 
 func ShowARound(content string) {
     
-    surface.Fill(sdl_window.screen, BGCOLOR) 
+	surface.Fill(sdl_window.screen, BGCOLOR) 
     
  	my_text := font.Render(sdl_window.main_font,content,true, TXTCOLOR,nil)
 
 	surface.Blit(sdl_window.screen,my_text,
-            draw.MidRect(Width/2,Height/2,surface.GetWidth(my_text),surface.GetHeight(my_text),Width,Height),nil)
+		draw.MidRect(Width/2,Height/2,surface.GetWidth(my_text),surface.GetHeight(my_text),Width,Height),nil)
 	
 	display.Flip()    
-            
-    sdl_window.EasingWindowBottom(Height)
-    
-    time.BlockDelay(DELAY_MS/2)
-    
-    sdl_window.EasingWindowTop(Height)
-    
-
+	
+	sdl_window.EasingWindowBottom(Height)
+	
+	time.BlockDelay(DELAY_MS/2)
+	
+	sdl_window.EasingWindowTop(Height)
     
 }
 
 func LoopCheckJobs(_dir string) {
-     defer func() {
-        // recover from panic if one occured. Set err to nil otherwise.
-        if (recover() != nil) {
-            err := errors.New("execute script errors")
-            fmt.Println(err)
-        }
-    }()
-    
-    counter := 0
-    
+	defer func() {
+		// recover from panic if one occured. Set err to nil otherwise.
+		if (recover() != nil) {
+			err := errors.New("execute script errors")
+			fmt.Println(err)
+		}
+	}()
+	
+	counter := 0
+
+	if Enabled == false { // check if disabled
+		fmt.Println("Disabled")
+		return
+	}
+	
 	if UI.FileExists(_dir) == false && UI.IsDirectory(_dir) == false {
 		return
 	}
+	
+	os.Chdir(_dir)
+	_dir = "."
+	var files []os.FileInfo
+	var err error 
+	for {
     
-    os.Chdir(_dir)
-    _dir = "."
-    var files []os.FileInfo
-    var err error 
-    for {
-    
-        if counter == 0 {
-            files,err = ioutil.ReadDir(_dir)
-            if err != nil {
-                log.Fatal(err)
-                return
-            }
-        }
-        
-        for _,f := range files {
-            
-            fname := _dir+"/"+f.Name()
-            if CheckScriptExt(fname) == false {
-                continue
-            }
-            
-            job_respond := RunScript( fname )
-            if job_respond!= nil {
-                job_respond_string := job_respond.String()
-                
-                if job_respond.Type == "once" {
-                    if val, ok := JobMap[_dir+"/"+f.Name()]; ok {
-                        
-                        if val != job_respond_string {
-                            JobMap[_dir+"/"+f.Name()] = job_respond_string
-                            ShowARound( job_respond.Content )
-                        }
-                    }else {
-                        JobMap[_dir+"/"+f.Name()] = job_respond_string
-                        ShowARound( job_respond.Content )
-                    }
-                }else if job_respond.Type == "repeat" {
-                    ShowARound( job_respond.Content )
-                }
-            }
-        }
-        
-        time.BlockDelay(DELAY_FREQ) 
-        
-        counter+=1
-        if counter >= SKIP_READ_DIR {
-            counter = 0
-        }
-    }
-    
+		if counter == 0 {
+			files,err = ioutil.ReadDir(_dir)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+		}
+		
+		for _,f := range files {
+			
+			fname := _dir+"/"+f.Name()
+			if CheckScriptExt(fname) == false {
+				continue
+			}
+			
+			job_respond := RunScript( fname )
+			if job_respond!= nil {
+				job_respond_string := job_respond.String()
+				
+				if job_respond.Type == "once" {
+					if val, ok := JobMap[_dir+"/"+f.Name()]; ok {
+						
+						if val != job_respond_string {
+							JobMap[_dir+"/"+f.Name()] = job_respond_string
+							ShowARound( job_respond.Content )
+						}
+					}else {
+						JobMap[_dir+"/"+f.Name()] = job_respond_string
+						ShowARound( job_respond.Content )
+					}
+				}else if job_respond.Type == "repeat" {
+					ShowARound( job_respond.Content )
+				}
+			}
+		}
+		
+		time.BlockDelay(DELAY_FREQ) 
+		
+		counter+=1
+		if counter >= SKIP_READ_DIR {
+			counter = 0
+		}
+	}
+	
 }
 
 func run() int {
@@ -405,16 +456,16 @@ func run() int {
 	sdl_window.screen = display.SetMode(int32(Width),int32(Height), 0 ,32)
 	sdl_window.win = display.GetWindow()
   
-    display.SetWindowPos(sdl_window.win,(320-Width)/2,-Height)
-    
-    display.SetWindowTitle(sdl_window.win,"GameShellNotify")
-    display.SetWindowBordered(sdl_window.win,false)
-    
+	display.SetWindowPos(sdl_window.win,(320-Width)/2,-Height)
+	
+	display.SetWindowTitle(sdl_window.win,"GameShellNotify")
+	display.SetWindowBordered(sdl_window.win,false)
+	
 	surface.Fill(sdl_window.screen, &color.Color{255,255,255,255} ) 
-
+	
 	fmt.Println(sdl_window.screen.Pitch)
 	fmt.Println(sdl_window.screen.BytesPerPixel() )
-
+	
 	font.Init()
 	
 	font_path := "/home/cpi/apps/launcher/skin/default/truetype/NotoSansCJK-Regular.ttf"
@@ -423,29 +474,29 @@ func run() int {
 	}
 	notocjk := font.Font(font_path,FTSIZE)
 	fmt.Println( font.LineSize( notocjk ))
-    sdl_window.main_font = notocjk
-    
+	sdl_window.main_font = notocjk
+	
  	my_text := font.Render(notocjk,"AirPlane Mode",true, &color.Color{234,123,12,255},nil)
-
+	
 	surface.Blit(sdl_window.screen,my_text,
-            draw.MidRect(Width/2,Height/2,surface.GetWidth(my_text),surface.GetHeight(my_text),Width,Height),nil)
+		draw.MidRect(Width/2,Height/2,surface.GetWidth(my_text),surface.GetHeight(my_text),Width,Height),nil)
 	
 	display.Flip()
-
-    event.AddCustomEvent(RUNEVT)
-    
-    sdl_window.UpdateWindowPos()
-    
-    go LoopCheckJobs("Jobs")
-    
-    
-    syswminfo,_ := sdl_window.win.GetWMInfo()
-    x11info := syswminfo.GetX11Info()
-    fmt.Println("x11info: ",x11info.Window )
-    
+	
+	event.AddCustomEvent(RUNEVT)
+	
+	sdl_window.UpdateWindowPos()
+	
+	go LoopCheckJobs("Jobs")
+	
+	
+	syswminfo,_ := sdl_window.win.GetWMInfo()
+	x11info := syswminfo.GetX11Info()
+	fmt.Println("x11info: ",x11info.Window )
+	
 	running := true
-    
-
+	
+	
 	for running {
 		ev := event.Wait()
 		if ev.Type == event.QUIT {
@@ -459,27 +510,27 @@ func run() int {
 		if ev.Type == event.KEYDOWN {
 			fmt.Println(ev)
 			if ev.Data["Key"] == "Q" {
-                gogame.Quit()
+				gogame.Quit()
 				return 0
 			}
 			if ev.Data["Key"] == "Escape" {
 				return 0
 			}
 			if ev.Data["Key"] == "L" {
-                sdl_window.EasingWindowLeft(100)
+				sdl_window.EasingWindowLeft(100)
 			}
 			if ev.Data["Key"] == "R" {
-                sdl_window.EasingWindowRight(100)
+				sdl_window.EasingWindowRight(100)
 			} 
 			if ev.Data["Key"] == "T" {
-                sdl_window.EasingWindowTop(40)
+				sdl_window.EasingWindowTop(40)
 			}                        
 			if ev.Data["Key"] == "P" {				
 				event.Post(RUNEVT,"GODEBUG=cgocheck=0 sucks") // just id and string, simplify the stuff
 			}
 		}
 	}
-
+	
 	return 0
 }
 
@@ -489,27 +540,84 @@ func SearchAndDestory(pidFile string) {
 		if pid, err := strconv.Atoi(string(piddata)); err == nil {
 			if process, err := os.FindProcess(pid); err == nil {
 				process.Kill()
-                ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
-                return
+				ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
+				return
 			}
 		}
 	}
-    
+	
 	ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
-    return
+	return
 }
 
+func HandleCMD(c net.Conn) {
+	
+	for {
+		buf := make([]byte, 512)
+		nr, err := c.Read(buf)
+		if err != nil {
+			return
+		}
+
+		data := buf[0:nr]
+		println("Got:", string(data))
+		data_str := strings.TrimRight(string(data),"\r\n")
+		
+		if data_str == "Enable" {
+			fmt.Println("Enable gsnotify")
+			Enabled = true
+			WriteConfig()
+		}
+		
+		if data_str == "Disable" {
+			fmt.Println("Disable gsnotify now")
+			Enabled = false
+			WriteConfig()
+		}
+		
+		_, err = c.Write(data)		
+		if err != nil {
+			log.Fatal("Writing client error: ", err)
+		}
+	}	
+}
+
+func InitSocket() {
+	cmdName := "rm"
+	cmdArgs := []string{"-rf","/tmp/gsnotify.sock"}
+
+	if _, err := exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error binding unix socket to /tmp/gsnotify.sock ", err)
+		return
+	}
+	
+	ln, err := net.Listen("unix", "/tmp/gsnotify.sock")
+	if err != nil {
+		log.Fatal("Listen error: ", err)
+	}
+
+	for {
+		fd, err := ln.Accept()
+		if err != nil {
+			log.Fatal("Accept error: ", err)
+		}
+		
+		go HandleCMD(fd)
+	}
+}
 
 
 func main() {
 	var exitcode int
         
 	os.Setenv("GODEBUG", "cgocheck=0")
+
+	InitSocket()
 	
-    SearchAndDestory("/tmp/gsnotify.pid")
+	SearchAndDestory("/tmp/gsnotify.pid")
     
 	sdl.Main(func() {
-        time.BlockDelay(DELAY_FREQ)
+		time.BlockDelay(DELAY_FREQ)
 		exitcode = run()
 	})
 
